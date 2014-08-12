7d1047962d970403a0a9f615e61b7787686d54c8
彭毅 <pengyi@ofcard.com>
2014-08-12 10:11:30 +0800

    增加udp调试信息

--- src/main/scala/com/qianmi/bugatti/actors/LogUdpActor.scala
+++ src/main/scala/com/qianmi/bugatti/actors/LogUdpActor.scala
@@ -21,6 +21,7 @@ class LogUdpActor(commandsActor: ActorRef, ipAddress: String, port: Int) extends
   def ready(socket: ActorRef): Receive = {
     case Udp.Received(data, remote) => {
       val logStr = data.decodeString("utf-8").dropRight(1) // 删除行尾的\0
+      log.debug(s"udp receive: ${logStr}")
       if (logStr.contains("Executing command")){
         val jid = logStr.replaceAll("""^.* with jid +(\d{20})""", "$1")
         if (jid.size == 20) {
